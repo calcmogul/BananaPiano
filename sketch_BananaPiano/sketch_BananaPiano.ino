61e4fda7797064a08c9e7e2d28ebd652104943b5
Tyler Veness <calcmogul@gmail.com>
2015-04-29 17:22:37 -0700

    arduino: defined wire protocol

--- sketch_BananaPiano/sketch_BananaPiano.ino
+++ sketch_BananaPiano/sketch_BananaPiano.ino
@@ -1,26 +1,21 @@
 void setup() {
-  Serial.begin(115200);
-  
-  for (int i = 22; i <= 26; i++) {
-    pinMode(i, INPUT);
-  }
+    Serial.begin(115200);
+
+    for (int i = 0; i <= 11; i++) {
+        pinMode(i, INPUT);
+    }
 }
 
 void loop() {
-  if (PINA & (1 << 22)) {
-    Serial.println("A");
-  }
-  if (PINA & (1 << 23)) {
-    Serial.println("B");
-  }
-  if (PINA & (1 << 24)) {
-    Serial.println("C");
-  }
-  if (PINA & (1 << 25)) {
-    Serial.println("D");
-  }
-  if (PINA & (1 << 26)) {
-    Serial.println("E");
-  }
+    // C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+    for (int i = 0; i <= 11; i++) {
+        if (analogRead(i) > 2.0) {
+            Serial.print("1");
+        }
+        else {
+            Serial.print("0");
+        }
+    }
+    Serial.println("");
 }
 
