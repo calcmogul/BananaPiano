731679ab33e6c0bc31401345a03edf0a84abce86
Tyler Veness <calcmogul@gmail.com>
2015-04-29 22:46:53 -0700

    arduino: wired analog pins and editted code to use them

--- sketch_BananaPiano/sketch_BananaPiano.ino
+++ sketch_BananaPiano/sketch_BananaPiano.ino
@@ -7,13 +7,13 @@ void setup() {
 }
 
 void loop() {
-    // C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+    // G#, A, Bb, B, C, C#, D, Eb, E, F, F#, G
     for (int i = 0; i <= 11; i++) {
-        if (analogRead(i) > 2.0) {
-            Serial.print("1");
+        if (analogRead(i) > 3.0) {
+            Serial.print("0");
         }
         else {
-            Serial.print("0");
+            Serial.print("1");
         }
     }
     Serial.println("");
