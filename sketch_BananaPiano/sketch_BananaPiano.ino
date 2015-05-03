aa4fd54b9eb56bbf7c23d3448857384bab8e7343
Tyler Veness <calcmogul@gmail.com>
2015-05-02 17:15:15 -0700

    arduino: each input pin is now provided with a separate 5V supply from a DIO pin

--- sketch_BananaPiano/sketch_BananaPiano.ino
+++ sketch_BananaPiano/sketch_BananaPiano.ino
@@ -2,7 +2,12 @@ void setup() {
     Serial.begin(115200);
 
     for (int i = 0; i <= 11; i++) {
-        pinMode(i, INPUT_PULLUP);
+        pinMode(i, INPUT);
+    }
+
+    for (int i = 42; i <= 53; i++) {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, HIGH);
     }
 }
 
@@ -10,7 +15,7 @@ void loop() {
     // G#, A, Bb, B, C, C#, D, Eb, E, F, F#, G
     for (int i = 0; i <= 11; i++) {
         // '1' means pin is HIGH; '0' means pin is LOW
-        if (analogRead(i) * 0.0049 > 3.0) {
+        if (analogRead(i) * 0.0049 > 3.5) {
             Serial.write('1');
         }
         else {
@@ -19,6 +24,6 @@ void loop() {
     }
     Serial.write('\n');
 
-    delay(50);
+    delay(40);
 }
 
