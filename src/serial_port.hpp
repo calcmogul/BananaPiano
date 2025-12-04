1ffb3b1b75f108ccc3a5ca9e6f52a28768ed4129
Tyler Veness <calcmogul@gmail.com>
2025-12-03 17:11:14 -0800

    Replace multiline comments with triple forward slashes

--- src/serial_port.hpp
+++ src/serial_port.hpp
@@ -13,54 +13,40 @@
 #undef NOMINMAX
 #endif
 
-/**
- * Provides an interface for communicating with an Arduino via the serial port
- */
+/// Provides an interface for communicating with an Arduino via the serial port
 class SerialPort {
 public:
     ~SerialPort() { disconnect(); }
 
-    /**
-     * Initialize serial communication over the given port.
-     *
-     * @param port_name Serial port name.
-     */
+    /// Initialize serial communication over the given port.
+    ///
+    /// @param port_name Serial port name.
     void connect(std::string_view port_name);
 
-    /**
-     * Closes the connection.
-     *
-     * NOTE: For some reason, after calling this functionm, the executable needs
-     * to be rerun to reconnect.
-     */
+    /// Closes the connection.
+    ///
+    /// NOTE: For some reason, after calling this functionm, the executable
+    /// needs to be rerun to reconnect.
     void disconnect();
 
-    /**
-     * Reads data into a buffer.
-     *
-     * @param buffer Destination buffer.
-     * @param num_chars Number of characters to read.
-     * @return The number of characters actually read or -1 on failure.
-     */
+    /// Reads data into a buffer.
+    ///
+    /// @param buffer Destination buffer.
+    /// @param num_chars Number of characters to read.
+    /// @return The number of characters actually read or -1 on failure.
     int read(char* buffer, size_t num_chars);
 
-    /**
-     * Writes data to the serial connection.
-     *
-     * @param buffer Source buffer.
-     * @param num_chars Number of characters to write.
-     * @return True on success, false on failure.
-     */
+    /// Writes data to the serial connection.
+    ///
+    /// @param buffer Source buffer.
+    /// @param num_chars Number of characters to write.
+    /// @return True on success, false on failure.
     bool write(char* buffer, size_t num_chars);
 
-    /**
-     * Returns true if serial port is connected.
-     */
+    /// Returns true if serial port is connected.
     bool is_connected() const { return m_connected; }
 
-    /**
-     * Returns list of serial port names.
-     */
+    /// Returns list of serial port names.
     static std::vector<std::string> get_serial_ports();
 
 private:
@@ -75,6 +61,6 @@ private:
     /// Serial port name.
     std::string m_port_name;
 
-    // Connection status.
+    /// Connection status.
     bool m_connected = false;
 };
